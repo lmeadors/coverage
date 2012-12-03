30192db693af35b87e2d750705cf10f393817f0d
Larry Meadors <larry.meadors@gmail.com>
2012-12-03 09:06:12 -0700

    Baseline - before adding magic bits for emma

--- coverage/src/main/java/com/lmeadors/HelloAndroidActivity.java
+++ coverage/src/main/java/com/lmeadors/HelloAndroidActivity.java
@@ -6,14 +6,8 @@ import android.util.Log;
 
 public class HelloAndroidActivity extends Activity {
 
-    private static String TAG = "coverage";
+    private static final String TAG = HelloAndroidActivity.class.getName();
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
