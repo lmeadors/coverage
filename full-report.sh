31fd2d3e23020e1f4fabc03fdd55a37c3893417d
Larry Meadors <larry.meadors@gmail.com>
2012-12-07 13:53:47 -0700

    SUCCESS!!

--- full-report.sh
+++ full-report.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-EMMA_JAR=/Users/lmeadors/.m2/repository/emma/emma/2.1.5320/emma-2.1.5320.jar
+EMMA_JAR=~/.m2/repository/emma/emma/2.1.5320/emma-2.1.5320.jar
 
 cd coverage/target/emma
 
 java -cp $EMMA_JAR emma report -r html -in coverage.em,coverage.ec -sp ../../src/main/java/
 
-#open application/target/emma/coverage/index.html
+open coverage/index.html
